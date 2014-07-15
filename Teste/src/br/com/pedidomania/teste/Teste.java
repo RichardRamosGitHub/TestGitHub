ff8937cf20393f5cae9fdb74b033fa5f69637ebe
Fabiano Leite <Fabiano Leite@FabianoLeite-PC>
2014-07-14 21:20:30 -0300

    nova mudança

--- Teste/src/br/com/pedidomania/teste/Teste.java
+++ Teste/src/br/com/pedidomania/teste/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		System.out.println("Bem vindos ao Pedido Mania! Teste Egit");
+		System.out.println("Bem vindos ao Pedido Mania! Teste Egit!!! Teste");
 
 	}
 
